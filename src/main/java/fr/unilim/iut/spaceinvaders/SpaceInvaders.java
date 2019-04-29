63937be78c2853647837b3cc0293d22e85436996
Kevin Poteau <kevin.poteau@etu.unilim.fr>
2019-04-29 16:22:34 +0200

    Classe SpaceInvader et Test fonctionnelle (2 tests)

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -2,24 +2,55 @@
   
     public class SpaceInvaders {
 
-	    int longueur;
+	    private static final char MARQUE_VIDE = '.';
+		private static final char MARQUE_VAISSEAU = 'V';
+		private static final char MARQUE_FIN_LIGNE = '\n';
+		int longueur;
 	    int hauteur;
+	    Vaisseau vaisseau;
 
 	    public SpaceInvaders(int longueur, int hauteur) {
 		   this.longueur = longueur;
 		   this.hauteur = hauteur;
 	   }
 	    
+	    public void positionnerUnNouveauVaisseau(int x, int y) {
+	        Vaisseau vaisseau = new Vaisseau (x,y);
+		}
+	    
+		private boolean aUnVaisseauQuiOccupeLaPosition(int y, int x) {
+			return aUnVaisseau() && vaisseau.occupeLaPosition(x, y);
+		}
+		
+	    private char recupererMarqueDeLaPosition(int x, int y) {
+	        char marque;
+	        if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
+	           marque=MARQUE_VAISSEAU;
+	        else
+	           marque=MARQUE_VIDE;
+	        return marque;
+		}
+	    
+		private boolean aUnVaisseau() {
+			return vaisseau!=null;
+		}
+		
 	    @Override
-	    public String toString() {
-	        String s = "";
-	        for (int i = 0; i < hauteur; i++) {
-	            for (int j = 0; j < longueur; j++) {
-	                s += ".";
-	            }
-	            s += "\n";
-	        }
-	        return s;
-	    }
+		public String toString() {
+			return recupererEspaceJeuDansChaineASCII();
+		}
+
+		private String recupererEspaceJeuDansChaineASCII() {
+			StringBuilder espaceDeJeu = new StringBuilder();
+			for (int y = 0; y < hauteur; y++) {
+				for (int x = 0; x < longueur; x++) {
+					espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
+				}
+				espaceDeJeu.append('\n');
+			}
+			return espaceDeJeu.toString();
+		}
    }
+    
+    
     
--- src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
+++ src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
@@ -1,4 +1,4 @@
- package fr.unilim.iut.spaceinvaders;
+    package fr.unilim.iut.spaceinvaders;
 
     import static org.junit.Assert.assertEquals;
     import org.junit.Test;
@@ -20,6 +20,23 @@
 		    "...............\n" + 
 		    "...............\n" , spaceinvaders.toString());
 	        }
-
+	   
+		@Test
+		public void test_unNouveauVaisseauEstCorrectementPositionneDansEspaceJeu() {
+			SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+			spaceinvaders.positionnerUnNouveauVaisseau(7,9);
+			assertEquals("" + 
+			"...............\n" + 
+			"...............\n" +
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			".......V.......\n" , spaceinvaders.toString());
+		}
+		
        }
-    
+   
